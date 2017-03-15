473c96e4cdc91e933b498da99463ce1215ca7f81
Tyler Coffey <tjcoffey+github@gmail.com>
2017-03-14 18:59:26 -0700

    Basic working parser for csv files. parseFile generates a string of a placemark with a LineString in it, then createKML wraps it to stuff all the placemarks into a single KML file.

---
+++ FR24toKML.py
@@ -0,0 +1,31 @@
+import csv
+import os
+
+def parseFile(csvInput):
+        with open(csvInput) as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+
+                placemark = '<Placemark>\n<name>' + csvInput + '</name>\n<LineString>\n<altitudeMode>absolute</altitudeMode>\n<coordinates>'
+
+                next(reader)
+                for row in reader:
+                        placemark += row[3].split(',')[1] + ',' + row[3].split(',')[0] + ',' + '{:.2f}'.format(int(row[4]) * 0.3048) + '\n'
+
+                placemark += '</coordinates>\n</LineString>\n</Placemark>'
+                return placemark
+
+def createKML(name, csvDirectory):
+        kmlFilename = name + '.kml'
+        kmlText = '<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n<name>' + kmlFilename +'</name>'
+        
+        for file in os.listdir(csvDirectory):
+                if file.endswith('.csv'):
+                        kmlText += parseFile(csvDirectory + file)
+
+        kmlText += '</Document>\n</kml>'
+        kmlFile = open(kmlFilename, 'w')
+        kmlFile.truncate()
+        kmlFile.write(kmlText)
+        kmlFile.close()
+
+createKML('test','../flightdata/')
